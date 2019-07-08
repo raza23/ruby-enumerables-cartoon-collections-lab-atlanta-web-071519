8f6935ee3152c8464d6fd621105055656a72c10b
Raza Tahir Shareef <rtshareef@gmail.com>
2019-07-08 07:24:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index{|value,index| puts "#{index} #{value}
 end
 
 def summon_captain_planet# code an argument here
